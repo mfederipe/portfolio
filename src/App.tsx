1527917b62f11195bd1e7e95a774cb13622538fb
Marison Federipe <marisonfdrp@gmail.com>
2025-04-04 16:41:36 -0700

    Add Header and Footer to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.scss";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
