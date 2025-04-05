256f382beb90a54748aa6338181bd351dbe7b0df
Marison Federipe <marisonfdrp@gmail.com>
2025-04-04 19:43:42 -0700

    Create Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
       <Footer />
     </>
   );
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,24 @@
+import "./Home.scss";
+import profile from "../../assets/images/profile.jpg";
+
+function Home() {
+  return (
+    <section className="home">
+      <div className="home__card">
+        <img src={profile} alt="Marison" className="home__avatar" />
+        <h1 className="home__name">const Marison = "Full-Stack Developer";</h1>
+        <p className="home__bio">
+          Hi, I'm Marison, a Software Engineer with a background in Health
+          Sciences. I fell in love with tech when I realized I could bring any
+          idea to life. I was always creative growing up, but somewhere along
+          the way, I lost that spark. Coding has helped me reconnect with that
+          side of myself, and that's what keeps me motivated each day.
+          <br />
+          Thanks for stopping by!ㅤ♡
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
