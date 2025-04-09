9dd795b4f74091207da614cf7af083a5d7917af5
Marison Federipe <marisonfdrp@gmail.com>
2025-04-08 20:56:48 -0700

    Create Contact component with styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Home />
       <Projects />
+      <Contact />
       <Footer />
     </>
   );
---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,27 @@
+import tapeIcon from "../../assets/icons/tape.svg";
+import "./Contact.scss";
+
+const Contact: React.FC = () => {
+  return (
+    <section className="contact">
+      <img
+        className="contact__tape-icon contact__tape-icon--left"
+        src={tapeIcon}
+        alt="tape icon"
+      />
+      <img
+        className="contact__tape-icon contact__tape-icon--right"
+        src={tapeIcon}
+        alt="tape icon"
+      />
+
+      <h2 className="contact__title">Let's Connect!</h2>
+      <p className="contact__info">
+        I'm always open to new opportunities, feel free to reach out:{" "}
+        <a href="mailto:johndoe@email.com">johndoe@email.com</a>
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
